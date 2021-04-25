4d68ab6143269ef8afe6ff05834d3592fc42d13b
André <tdc@gmx.ch>
2021-04-25 14:42:36 +0200

    spark fix

--- sparkStreaming.py
+++ sparkStreaming.py
@@ -1,4 +1,13 @@
 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+
+spark = SparkSession \
+    .builder \
+    .appName("SreamingProzessor") \
+    .getOrCreate()
+
 lines = spark \
     .readStream \
     .format("socket") \
