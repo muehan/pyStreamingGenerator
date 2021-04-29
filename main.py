6d2fde3da9953456c32df1d3d666a6d83fcffa47
André <tdc@gmx.ch>
2021-04-29 18:28:27 +0200

    send hello world

--- main.py
+++ main.py
@@ -12,24 +12,10 @@ i = 0
 
 serversocket.bind((host, port))
 
-class client(Thread):
-    def __init__(self, socket, address):
-        print('client established')
-        Thread.__init__(self)
-        self.sock = socket
-        self.addr = address
-        self.start()
-
-    def run(self):
-        while 1:
-            print('Client sent:', self.sock.recv(1024).decode())
-            self.sock.send(b'Oi you sent something to me')
-
 serversocket.listen(5)
 print ('server started and listening')
 while 1:
     clientsocket, address = serversocket.accept()
-    client(clientsocket, address)
     
     for j in range(50):
         print("send hello world")
