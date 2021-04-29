282d75bef96d70d79d4c59e1be2f6ef4afd54960
André <tdc@gmx.ch>
2021-04-29 20:20:52 +0200

    process stream

--- sparkStreaming.py
+++ sparkStreaming.py
@@ -1,7 +1,41 @@
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
+from pyspark.sql.types import StructType, StructField, TimestampType, StringType, IntegerType, DoubleType, DateType
+from pyspark.sql.functions import unix_timestamp, to_date, col, from_unixtime, udf, year, month, hour, minute, to_date
+from datetime import datetime
+
+# ---- Custom function ----
+def extractDate(t):
+    dt = datetime.strptime(t, "%Y-%m-%d %H:%M:%S")
+    return dt.strftime('%Y-%m-%d')
+    
+def calculateResponseTime(count):
+    return 60 / count
+    
+def getDateTime(date, hour, minute):
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+    time_obj = datetime.strptime(str(hour)+":"+str(minute), '%H:%M').time()
+    return datetime.combine(date_obj, time_obj)
+
+schema = StructType([ \
+    StructField("code", StringType()), \
+    StructField("client_id", IntegerType()), \
+    StructField("loc_ts", IntegerType()), \
+    StructField("length", IntegerType()), \
+    StructField("op", StringType()), \
+    StructField("err_code", IntegerType()), \
+    StructField("time", StringType()), \
+    StructField("thread_id", IntegerType())])
+
+udf_getDate = udf(extractDate, StringType())
+udf_calculateResponseTime = udf(calculateResponseTime, DoubleType())
+udf_getDateTime = udf(getDateTime, TimestampType())
+
+spark.udf.register("udf_getDate", udf_getDate)
+spark.udf.register("udf_calculateResponseTime", udf_calculateResponseTime)
+spark.udf.register("udf_getDateTime", udf_getDateTime)
+
+# ---- End Custom function ----
 
 spark = SparkSession \
     .builder \
@@ -15,21 +49,28 @@ lines = spark \
     .option("port", 9999) \
     .load()
 
-words = lines.select(
-    explode(
-        split(lines.value, " ")
-    ).alias("word")
-)
+df_withTime = lines.withColumn("datetime", from_unixtime(col("time")[0:10]))
 
-wordCounts = words \
-    .groupBy("word") \
-    .count()
+df_withMinuteAndHour = df_withTime \
+    .withColumn("minute", minute("datetime")) \
+    .withColumn("hour", hour("datetime")) \
+    .withColumn("month", month("datetime")) \
+    .withColumn("date", udf_getDate("datetime")) \
+    .withColumn("year", year("datetime"))
+    
+df_filtered = df_withMinuteAndHour.filter(col("code") == "res_snd")
 
-query = wordCounts \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start()
+df_result = df_filtered.groupBy("date", "month", "year", "hour", "minute").count()
 
-query \
-    .awaitTermination()
+df_result_calc = df_result \
+    .withColumn("responseTime", udf_calculateResponseTime("count")) \
+    .withColumn("datetime", udf_getDateTime("date", "hour", "minute"))
+    
+df_result_cleaned = df_result_calc \
+    .drop("date") \
+    .drop("hour") \
+    .drop("minute")
+
+df_result_cleaned.sort("datetime").show(300,False)
+
+df_result_cleaned.registerTempTable("streamTemp")
