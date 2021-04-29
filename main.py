7b32f72c3c35f0f6d567e05b4bc745908ff51ad2
André <tdc@gmx.ch>
2021-04-29 18:16:30 +0200

    send hello world

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'  # The server's hostname or IP address
 port = 9999         # The port used by the server
 
+i = 0
+
 serversocket.bind((host, port))
 
 class client(Thread):
@@ -29,7 +31,7 @@ while 1:
     client(clientsocket, address)
     
     for j in range(10):
-        clientsocket.send(bytes("{Hallo World}\n".format(i), "utf-8"))
+        clientsocket.send(bytes("Hallo World", "utf-8"))
         i += 1
         time.sleep(1)
     clientsocket.close()
