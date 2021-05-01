49613d52035013c790a6ec795a696915157a5c7a
André <tdc@gmx.ch>
2021-05-01 11:25:25 +0200

    try read stream

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 from threading import *
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = '127.0.0.1'
 port = 9999
 
